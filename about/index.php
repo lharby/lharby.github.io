1072cb7bad7166d97b353641c7daa122125826f4
Luke Harby <luke.harby@contractor.isbanuk.com>
2016-12-20 16:29:08 +0000

    Main commit, clean this up afterwards

--- about/index.php
+++ about/index.php
@@ -1,7 +1,13 @@
 <?php include "../common/header.php"; ?>
 
 <div id="page-content">
-    <h1><span class="header">About Us</span</h1>
+    <h1><span class="header">About Us</span></h1>
+    <ul class="nav clearfix">
+    	<li><a href="#10.jpg"><img src="http://slackwise.org.uk/photography/images/cafe.jpg"/></a></li>
+    	<li><a href="#11.jpg"><img src="http://slackwise.org.uk/photography/images/bb.jpg"/></a></li>
+    	<li><a href="#12.jpg"><img src="http://slackwise.org.uk/photography/images/snowtracks.jpg"/></a></li>
+    </ul>
+    <a href="#" class="close">X</a>
 </div>
 
 <?php include "../common/footer.php"; ?>
--- common/common-functions.php
+++ common/common-functions.php
@@ -1,99 +1,42 @@
 <?php
 
-    // get the document location
-    $strPageURL = $_SERVER['SCRIPT_NAME'];
-    // get array of page URL and split by '/'
-    $arrPageURL = explode("/", $strPageURL);
-    
-    // get the filename
-    $strFileName = $arrPageURL[count($arrPageURL)];
-    // split the filename by . (remove extension .php .html)
-    $arrFileName = explode(".", $strFileName);
-    // get the first index of the file name
-    $strFileName = $arrFileName[1];
-    
-    if(count($arrPageURL) > 1){
-        // get penultimate array value 
-        $strDirectoryParent = $arrPageURL[count($arrPageURL)-1];
-    }else{
-        $strDirectoryParent = $strFileName;
-    }
-    
-    if(count($arrPageURL)>=1){
-        // get the primary directory 
-        $primaryDir = $arrPageURL[1];
-    }else{
-        $primaryDir = "";
-    }
-    
-    if(count($arrPageURL)>=2){
-        // get the secondary directory
-        $secondaryDir = $arrPageURL[2];
-    }else{
-        $secondaryDir = "";
-    }
-    
-    if(count($arrPageURL)>=3){
-        // get the tertiary directory
-        $tertiaryDir = $arrPageURL[3];
-    }else{
-        $tertiaryDir = "";
-    }
-    
-    // set default variable values
-    $siteTitle = "Hogarth";
+ // set default variables
     $strTitle = "";
-    
-    // primary directory switch statement
-    switch (strtolower($primaryDir)) {
-    
-    case 'index':
-        $strTitle = 'Home';
-        break;
-        
-    case 'about':
-        $strTitle = 'About&nbsp;Us';
-        break;
-    
-    case 'contact':
-        $strTitle = 'Contact&nbsp;Us';
-        break;
-    
-    case 'downloads':
-        $strTitle = 'Downloads';
-        break;
-    
-    case 'products':
-        $strTitle = 'Products';
-        break;
-    
-    case 'terms':
-        $strTitle = 'Terms&nbsp;&amp;&nbsp;Conditions';
-        break;
+    $primaryDir = "";
+    $secondaryDir = "";
+    $section = "";
 
-    }
+    // get root directory
+    $strPageURL = $_SERVER["SCRIPT_NAME"];
+    // split the pathname by '/'
+    $strFileName = explode ('/', $strPageURL);
+    // get the primary folder
+    $section = $strFileName[2];
+    // get the root filename
+    $strIndexFile =  basename($strPageURL);
     
-    // set default variable value
-    $strTitle2 = "";
-    
-    // secondary directory switch statement 
-    switch (strtolower($secondaryDir)) {
-        
-        case 'cm':
-            $strTitle2 = 'Change&nbsp;Management';
-            break;
-        
-        case 'fido':
-            $strTitle2 = 'Fido';
-            break;
-        
-        case 'zonza':
-            $strTitle2 = 'Zonza';
-            break;
-        
-        case 'quagga':
-            $strTitle2 = 'Quagga';
-            break;
+    $siteTitle = "Hogarth";
+    // now for the switch statement, evaluate in lower case
+    switch (strtolower($section)) {
+    case "index.php":
+        $strTitle = "";
+        break;
+    case "products":
+        $strTitle = " - Products";
+        break;
+    case "what":
+        $strTitle = " - What we do";
+        break;
+    case "products":
+        $strTitle = " - Products and services";
+        break;
+    case "contact":
+        $strTitle = " - Contact";
+        break;
+        // set a default
+    case "": 
+        echo "";
+        break;
     }
     
 ?>
