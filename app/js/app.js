99cc45bcf5d4125fb829ca9273793519cf6e7aeb
Luke Harby <Luke.Harby@akqa.com>
2022-08-17 15:31:31 +0100

    toggle contrast 9

--- app/js/app.js
+++ app/js/app.js
@@ -113,9 +113,9 @@ $(document).ready(function () {
 
 /***/ }),
 /* 1 */
-/***/ (function(module, exports) {
+/***/ (function(module, exports, __webpack_require__) {
 
-throw new Error("Module build failed (from ./node_modules/mini-css-extract-plugin/dist/loader.js):\nModuleBuildError: Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\nSassError: Undefined variable: \"$color--white\".\n        on line 14 of src/scss/components/_page-variants.scss\n        from line 7 of components/*.scss\n        from line 12 of C:\\Users\\Luke.Harby\\Documents\\LH\\sw\\sites\\lharby.github.io\\src\\scss\\main.scss\n>>             background-color: $color__white;\r\n   ------------------------------^\n\n    at C:\\Users\\Luke.Harby\\Documents\\LH\\sw\\sites\\lharby.github.io\\node_modules\\webpack\\lib\\NormalModule.js:316:20\n    at C:\\Users\\Luke.Harby\\Documents\\LH\\sw\\sites\\lharby.github.io\\node_modules\\loader-runner\\lib\\LoaderRunner.js:367:11\n    at C:\\Users\\Luke.Harby\\Documents\\LH\\sw\\sites\\lharby.github.io\\node_modules\\loader-runner\\lib\\LoaderRunner.js:233:18\n    at context.callback (C:\\Users\\Luke.Harby\\Documents\\LH\\sw\\sites\\lharby.github.io\\node_modules\\loader-runner\\lib\\LoaderRunner.js:111:13)\n    at Object.callback (C:\\Users\\Luke.Harby\\Documents\\LH\\sw\\sites\\lharby.github.io\\node_modules\\sass-loader\\dist\\index.js:73:7)\n    at Object.done [as callback] (C:\\Users\\Luke.Harby\\Documents\\LH\\sw\\sites\\lharby.github.io\\node_modules\\neo-async\\async.js:8069:18)\n    at options.error (C:\\Users\\Luke.Harby\\Documents\\LH\\sw\\sites\\lharby.github.io\\node_modules\\node-sass\\lib\\index.js:294:32)");
+// extracted by mini-css-extract-plugin
 
 /***/ }),
 /* 2 */
