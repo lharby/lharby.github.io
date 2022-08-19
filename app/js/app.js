88bde9bc0f4b8e962d79431fb407c08a9ec8ae3e
Luke Harby <Luke.Harby@akqa.com>
2022-08-19 09:37:18 +0100

    Add includes 8

--- app/js/app.js
+++ app/js/app.js
@@ -93,12 +93,12 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _scss_main_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(1);
 /* harmony import */ var _scss_main_scss__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_scss_main_scss__WEBPACK_IMPORTED_MODULE_0__);
 /* harmony import */ var _js_global__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(2);
-/* harmony import */ var _js_navigation__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(3);
-/* harmony import */ var _js_indexPage__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(4);
-/* harmony import */ var _js_date__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(5);
-/* harmony import */ var _js_toggleContrast__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(6);
-/* harmony import */ var _js_obfuscateEmail__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(7);
-/* harmony import */ var _js_appendForm__WEBPACK_IMPORTED_MODULE_7__ = __webpack_require__(8);
+/* harmony import */ var _js_navigation__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(4);
+/* harmony import */ var _js_indexPage__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(5);
+/* harmony import */ var _js_date__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(6);
+/* harmony import */ var _js_toggleContrast__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(7);
+/* harmony import */ var _js_obfuscateEmail__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(8);
+/* harmony import */ var _js_appendForm__WEBPACK_IMPORTED_MODULE_7__ = __webpack_require__(9);
 
 
 
@@ -155,228 +155,10 @@ var REMOTE_DOMAIN = "https://lharby.github.io/";
 var REMOTE_SERVER = process.env.NEXT_PUBLIC_REMOTE_SERVER;
 var REMOTE_PATH = "studio-malarkey";
 
-/* WEBPACK VAR INJECTION */}.call(this, __webpack_require__(9)))
+/* WEBPACK VAR INJECTION */}.call(this, __webpack_require__(3)))
 
 /***/ }),
 /* 3 */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-__webpack_require__.r(__webpack_exports__);
-function _toConsumableArray(arr) { return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _unsupportedIterableToArray(arr) || _nonIterableSpread(); }
-
-function _nonIterableSpread() { throw new TypeError("Invalid attempt to spread non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
-
-function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
-
-function _iterableToArray(iter) { if (typeof Symbol !== "undefined" && iter[Symbol.iterator] != null || iter["@@iterator"] != null) return Array.from(iter); }
-
-function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) return _arrayLikeToArray(arr); }
-
-function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
-
-/*
-Luke Harby
-slackwise LTD
-https://slackwise.org.uk
-2012 - present
-*/
-
-/* navigation */
-var navigation = function navigation() {
-  var wrapper = 'nav-wrapper';
-  var target = document.querySelector(".".concat(wrapper));
-  var overlay = document.querySelector('.overlay');
-  var trigger = document.querySelectorAll('.menu__toggle');
-  var overlayOpenClass = 'overlay__open';
-  var scrollLockClass = 'scroll-lock';
-
-  _toConsumableArray(trigger).forEach(function (item) {
-    item.addEventListener('click', function (event) {
-      event.preventDefault();
-
-      if (item.classList.contains('menu__toggle--close')) {
-        closeNavigation();
-      } else {
-        openNavigation();
-      }
-    });
-  });
-
-  overlay.addEventListener('click', function () {
-    if (overlay.classList.contains(overlayOpenClass)) {
-      closeNavigation();
-    }
-  });
-  window.addEventListener("keydown", function (event) {
-    if (event.key === "Escape" && overlay.classList.contains(overlayOpenClass)) {
-      closeNavigation();
-    }
-  });
-
-  var openNavigation = function openNavigation() {
-    target.classList.add("".concat(wrapper, "--open"));
-    document.body.classList.add(scrollLockClass);
-    overlay.classList.add(overlayOpenClass);
-  };
-
-  var closeNavigation = function closeNavigation() {
-    target.classList.remove("".concat(wrapper, "--open"));
-    document.body.classList.remove(scrollLockClass);
-    overlay.classList.remove(overlayOpenClass);
-  };
-};
-
-/* harmony default export */ __webpack_exports__["default"] = (navigation);
-
-/***/ }),
-/* 4 */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-__webpack_require__.r(__webpack_exports__);
-/*
-Luke Harby
-slackwise LTD
-https://slackwise.org.uk
-2012 - present
-*/
-
-/* index page */
-var indexPage = function indexPage() {
-  var dom = document.querySelector('html');
-  var url = document.location.pathname.split('/');
-  var primaryDir = url[1];
-  var secondaryDir = url[2];
-  var tertiaryDir = url[3];
-
-  if (!primaryDir) {
-    dom.classList.add('index');
-  } else if (tertiaryDir) {
-    dom.classList.add(primaryDir, secondaryDir, tertiaryDir);
-  } else if (secondaryDir) {
-    dom.classList.add(primaryDir, secondaryDir);
-  } else {
-    dom.classList.add(primaryDir);
-  }
-};
-
-/* harmony default export */ __webpack_exports__["default"] = (indexPage);
-
-/***/ }),
-/* 5 */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-__webpack_require__.r(__webpack_exports__);
-/*
-Luke Harby
-slackwise LTD
-https://slackwise.org.uk
-2012 - present
-*/
-
-/* date function */
-var date = function date() {
-  var dateWrapper = document.querySelector('.date-wrapper');
-  var getYear = new Date().getFullYear();
-  dateWrapper.textContent = getYear;
-};
-
-/* harmony default export */ __webpack_exports__["default"] = (date);
-
-/***/ }),
-/* 6 */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-__webpack_require__.r(__webpack_exports__);
-/* harmony import */ var _global__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(2);
-/*
-Luke Harby
-slackwise LTD
-https://slackwise.org.uk
-2012 - present
-*/
-
-/* toggle contrast function */
-
-
-var toggleContrast = function toggleContrast() {
-  var trigger = document.querySelector('.toggle-contrast');
-  var contrastClass = 'high-contrast';
-  var contrastIsSet = window.localStorage.getItem(contrastClass);
-  contrastIsSet === '1' ? _global__WEBPACK_IMPORTED_MODULE_0__["DOM"].classList.add(contrastClass) : null;
-  trigger.addEventListener('click', function (event) {
-    event.preventDefault();
-
-    if (_global__WEBPACK_IMPORTED_MODULE_0__["DOM"].classList.contains(contrastClass)) {
-      _global__WEBPACK_IMPORTED_MODULE_0__["DOM"].classList.remove(contrastClass);
-      window.localStorage.removeItem(contrastClass);
-    } else {
-      _global__WEBPACK_IMPORTED_MODULE_0__["DOM"].classList.add(contrastClass);
-      window.localStorage.setItem(contrastClass, '1');
-    }
-  });
-};
-
-/* harmony default export */ __webpack_exports__["default"] = (toggleContrast);
-
-/***/ }),
-/* 7 */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-__webpack_require__.r(__webpack_exports__);
-/* harmony import */ var _global__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(2);
-/*
-Luke Harby
-slackwise LTD
-https://slackwise.org.uk
-2012 - present
-*/
-
-/* obfuscate email */
-
-
-var obfuscateEmail = function obfuscateEmail() {
-  var trigger = document.querySelector('.email-address');
-  trigger.addEventListener('click', function () {
-    trigger.setAttribute('href', "mailto:".concat(_global__WEBPACK_IMPORTED_MODULE_0__["EMAIL_ADDRESS"]));
-  });
-};
-
-/* harmony default export */ __webpack_exports__["default"] = (obfuscateEmail);
-
-/***/ }),
-/* 8 */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-__webpack_require__.r(__webpack_exports__);
-/* harmony import */ var _global__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(2);
-/*
-Luke Harby
-slackwise LTD
-https://slackwise.org.uk
-2012 - present
-*/
-
-/* append form */
-
-var template = "\n    <form action=\"\" method=\"post\" class=\"form\">\n        <div class=\"form-control\">\n            <label class=\"el-4\" for=\"name\">Name</label>\n            <input class=\"el-5\" type=\"text\" name=\"name\" id=\"name\">\n        </div>\n        <div class=\"form-control\">\n            <label class=\"el-4\" for=\"email\">Email</label>\n            <input class=\"el-5\" type=\"text\" name=\"email\" id=\"email\"> \n        </div>\n        <div class=\"form-control\">\n            <label class=\"el-4\" for=\"contact-no\">Contact number</label>\n            <input class=\"el-5\" type=\"contact-no\" id=\"contact-no\">\n        </div>\n        <div class=\"form-control\">\n            <label class=\"el-4\" for=\"comments\">Comments</label>\n            <textarea class=\"el-5\" name=\"comments\" id=\"comments\" rows=\"5\"></textarea>\n        </div>\n        <div class=\"form-control\">\n            <div class=\"el-4\"></div>\n            <div class=\"el-5\">\n                <button type=\"submit\">Submit</button>\n            </div>\n        </div>\n    </form>\n";
-
-var appendForm = function appendForm() {
-  var wrapper = _global__WEBPACK_IMPORTED_MODULE_0__["DOM"].querySelector('.form-wrapper');
-  wrapper.innerHTML = template;
-  var form = wrapper.querySelector('.form');
-  form.setAttribute('action', "".concat(_global__WEBPACK_IMPORTED_MODULE_0__["REMOTE_SERVER"], "sites/").concat(_global__WEBPACK_IMPORTED_MODULE_0__["REMOTE_PATH"], "/includes/mailer.php"));
-};
-
-/* harmony default export */ __webpack_exports__["default"] = (appendForm);
-
-/***/ }),
-/* 9 */
 /***/ (function(module, exports) {
 
 // shim for using process in browser
@@ -564,6 +346,224 @@ process.chdir = function (dir) {
 };
 process.umask = function() { return 0; };
 
+
+/***/ }),
+/* 4 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+function _toConsumableArray(arr) { return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _unsupportedIterableToArray(arr) || _nonIterableSpread(); }
+
+function _nonIterableSpread() { throw new TypeError("Invalid attempt to spread non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
+
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+
+function _iterableToArray(iter) { if (typeof Symbol !== "undefined" && iter[Symbol.iterator] != null || iter["@@iterator"] != null) return Array.from(iter); }
+
+function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) return _arrayLikeToArray(arr); }
+
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
+
+/*
+Luke Harby
+slackwise LTD
+https://slackwise.org.uk
+2012 - present
+*/
+
+/* navigation */
+var navigation = function navigation() {
+  var wrapper = 'nav-wrapper';
+  var target = document.querySelector(".".concat(wrapper));
+  var overlay = document.querySelector('.overlay');
+  var trigger = document.querySelectorAll('.menu__toggle');
+  var overlayOpenClass = 'overlay__open';
+  var scrollLockClass = 'scroll-lock';
+
+  _toConsumableArray(trigger).forEach(function (item) {
+    item.addEventListener('click', function (event) {
+      event.preventDefault();
+
+      if (item.classList.contains('menu__toggle--close')) {
+        closeNavigation();
+      } else {
+        openNavigation();
+      }
+    });
+  });
+
+  overlay.addEventListener('click', function () {
+    if (overlay.classList.contains(overlayOpenClass)) {
+      closeNavigation();
+    }
+  });
+  window.addEventListener("keydown", function (event) {
+    if (event.key === "Escape" && overlay.classList.contains(overlayOpenClass)) {
+      closeNavigation();
+    }
+  });
+
+  var openNavigation = function openNavigation() {
+    target.classList.add("".concat(wrapper, "--open"));
+    document.body.classList.add(scrollLockClass);
+    overlay.classList.add(overlayOpenClass);
+  };
+
+  var closeNavigation = function closeNavigation() {
+    target.classList.remove("".concat(wrapper, "--open"));
+    document.body.classList.remove(scrollLockClass);
+    overlay.classList.remove(overlayOpenClass);
+  };
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (navigation);
+
+/***/ }),
+/* 5 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/*
+Luke Harby
+slackwise LTD
+https://slackwise.org.uk
+2012 - present
+*/
+
+/* index page */
+var indexPage = function indexPage() {
+  var dom = document.querySelector('html');
+  var url = document.location.pathname.split('/');
+  var primaryDir = url[1];
+  var secondaryDir = url[2];
+  var tertiaryDir = url[3];
+
+  if (!primaryDir) {
+    dom.classList.add('index');
+  } else if (tertiaryDir) {
+    dom.classList.add(primaryDir, secondaryDir, tertiaryDir);
+  } else if (secondaryDir) {
+    dom.classList.add(primaryDir, secondaryDir);
+  } else {
+    dom.classList.add(primaryDir);
+  }
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (indexPage);
+
+/***/ }),
+/* 6 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/*
+Luke Harby
+slackwise LTD
+https://slackwise.org.uk
+2012 - present
+*/
+
+/* date function */
+var date = function date() {
+  var dateWrapper = document.querySelector('.date-wrapper');
+  var getYear = new Date().getFullYear();
+  dateWrapper.textContent = getYear;
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (date);
+
+/***/ }),
+/* 7 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _global__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(2);
+/*
+Luke Harby
+slackwise LTD
+https://slackwise.org.uk
+2012 - present
+*/
+
+/* toggle contrast function */
+
+
+var toggleContrast = function toggleContrast() {
+  var trigger = document.querySelector('.toggle-contrast');
+  var contrastClass = 'high-contrast';
+  var contrastIsSet = window.localStorage.getItem(contrastClass);
+  contrastIsSet === '1' ? _global__WEBPACK_IMPORTED_MODULE_0__["DOM"].classList.add(contrastClass) : null;
+  trigger.addEventListener('click', function (event) {
+    event.preventDefault();
+
+    if (_global__WEBPACK_IMPORTED_MODULE_0__["DOM"].classList.contains(contrastClass)) {
+      _global__WEBPACK_IMPORTED_MODULE_0__["DOM"].classList.remove(contrastClass);
+      window.localStorage.removeItem(contrastClass);
+    } else {
+      _global__WEBPACK_IMPORTED_MODULE_0__["DOM"].classList.add(contrastClass);
+      window.localStorage.setItem(contrastClass, '1');
+    }
+  });
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (toggleContrast);
+
+/***/ }),
+/* 8 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _global__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(2);
+/*
+Luke Harby
+slackwise LTD
+https://slackwise.org.uk
+2012 - present
+*/
+
+/* obfuscate email */
+
+
+var obfuscateEmail = function obfuscateEmail() {
+  var trigger = document.querySelector('.email-address');
+  trigger.addEventListener('click', function () {
+    trigger.setAttribute('href', "mailto:".concat(_global__WEBPACK_IMPORTED_MODULE_0__["EMAIL_ADDRESS"]));
+  });
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (obfuscateEmail);
+
+/***/ }),
+/* 9 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _global__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(2);
+/*
+Luke Harby
+slackwise LTD
+https://slackwise.org.uk
+2012 - present
+*/
+
+/* append form */
+
+var template = "\n    <form action=\"\" method=\"post\" class=\"form\">\n        <div class=\"form-control\">\n            <label class=\"el-4\" for=\"name\">Name</label>\n            <input class=\"el-5\" type=\"text\" name=\"name\" id=\"name\">\n        </div>\n        <div class=\"form-control\">\n            <label class=\"el-4\" for=\"email\">Email</label>\n            <input class=\"el-5\" type=\"text\" name=\"email\" id=\"email\"> \n        </div>\n        <div class=\"form-control\">\n            <label class=\"el-4\" for=\"contact-no\">Contact number</label>\n            <input class=\"el-5\" type=\"contact-no\" id=\"contact-no\">\n        </div>\n        <div class=\"form-control\">\n            <label class=\"el-4\" for=\"comments\">Comments</label>\n            <textarea class=\"el-5\" name=\"comments\" id=\"comments\" rows=\"5\"></textarea>\n        </div>\n        <div class=\"form-control\">\n            <div class=\"el-4\"></div>\n            <div class=\"el-5\">\n                <button type=\"submit\">Submit</button>\n            </div>\n        </div>\n    </form>\n";
+
+var appendForm = function appendForm() {
+  var wrapper = _global__WEBPACK_IMPORTED_MODULE_0__["DOM"].querySelector('.form-wrapper');
+  wrapper.innerHTML = template;
+  var form = wrapper.querySelector('.form');
+  form.setAttribute('action', "".concat(_global__WEBPACK_IMPORTED_MODULE_0__["REMOTE_SERVER"]).concat(_global__WEBPACK_IMPORTED_MODULE_0__["REMOTE_PATH"], "/includes/mailer.php"));
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (appendForm);
 
 /***/ })
 /******/ ]);
