e30823449662a5a963cfaf752d56cef9b3cca6dc
Luke Harby <Luke.Harby@akqa.com>
2023-09-20 16:18:40 +0100

    Update transitions, and main page markup

--- app/js/app.js
+++ app/js/app.js
@@ -117,8 +117,9 @@ __webpack_require__.r(__webpack_exports__);
 
 
 
- // global
 
+
+// global
 $(document).ready(function () {
   Object(_js_utils__WEBPACK_IMPORTED_MODULE_2__["siteResponsiveness"])();
   Object(_js_navigation__WEBPACK_IMPORTED_MODULE_3__["default"])();
@@ -128,15 +129,12 @@ $(document).ready(function () {
   Object(_js_getScrollbarWidth__WEBPACK_IMPORTED_MODULE_10__["default"])();
   Object(_js_toggleContrast__WEBPACK_IMPORTED_MODULE_7__["default"])();
   Object(_js_obfuscateEmail__WEBPACK_IMPORTED_MODULE_8__["default"])();
-
   if (!_js_global__WEBPACK_IMPORTED_MODULE_1__["site"].isMobile) {
     Object(_js_lazyLoad__WEBPACK_IMPORTED_MODULE_11__["setImageSource"])();
   }
-
   if (_js_global__WEBPACK_IMPORTED_MODULE_1__["DOM"].classList.contains('contact')) {
     Object(_js_appendForm__WEBPACK_IMPORTED_MODULE_9__["default"])();
   }
-
   if (_js_global__WEBPACK_IMPORTED_MODULE_1__["DOM"].classList.contains('high-contrast')) {
     Object(_js_randomColours__WEBPACK_IMPORTED_MODULE_12__["setRandomColour"])();
     setTimeout(_js_randomColours__WEBPACK_IMPORTED_MODULE_12__["attachClickEvent"], 100);
@@ -172,6 +170,7 @@ https://slackwise.org.uk
 */
 
 /* global */
+
 var DOM = document.querySelector('html');
 var BODY = DOM.querySelector('body');
 var HIDDEN_CLASS = 'hidden';
@@ -211,6 +210,7 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "siteResponsiveness", function() { return siteResponsiveness; });
 /* harmony import */ var _global__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(2);
 
+
 /**
  * A bunch of utilities.
  *
@@ -223,10 +223,10 @@ __webpack_require__.r(__webpack_exports__);
  * @param {*} element - The element to check.
  * @returns {boolean} `true` if the given argument is an element (or document, or window), and `false` otherwise.
  */
-
 function isElement(element) {
   return element instanceof Element || element instanceof Document || element instanceof Window;
 }
+
 /**
  * Get the current scroll values of the given element (or window). Will return an object containing
  * "left" and "top" properties, which are set to the scroll values, or false if no compatible element
@@ -235,10 +235,8 @@ function isElement(element) {
  * @param {Element|Window} [element=window]
  * @returns {{ left: number, top: number } | boolean}
  */
-
 function getElementScroll() {
   var element = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : window;
-
   if (isElement(element)) {
     if (element instanceof Window) {
       return {
@@ -255,49 +253,52 @@ function getElementScroll() {
     return false;
   }
 }
+
 /*
  * Get the current state of the scroll lock. `true` if the scroll lock is enabled, otherwise `false`.
  *
  * @type {boolean}
  */
-
 var className = 'doc-scroll-lock';
 var scrollTop = 0;
 var isScrollLocked = false;
+
 /**
  * Enable the scroll lock.
  */
-
 function enableScrollLock() {
   if (!isScrollLocked) {
     // Get scroll position
-    var scrollPosition = getElementScroll(); // Reset scroll position
+    var scrollPosition = getElementScroll();
 
+    // Reset scroll position
     window.scrollTo(scrollPosition.left, 0);
     var htmlTag = document.documentElement;
     htmlTag.classList.add(className);
     htmlTag.style.marginTop = "".concat(-scrollPosition.top, "px");
     htmlTag.style.position = 'fixed';
     htmlTag.style.overflow = 'hidden';
-    htmlTag.style.width = '100%'; // Trigger event on target. You can listen for it using document.body.addEventListener("site.scrollLock:enable", callbackHere)
-    // triggerCustomEvent(document.body, "site.scrollLock:enable");
-    // Remember state
+    htmlTag.style.width = '100%';
 
+    // Trigger event on target. You can listen for it using document.body.addEventListener("site.scrollLock:enable", callbackHere)
+    // triggerCustomEvent(document.body, "site.scrollLock:enable");
+
+    // Remember state
     isScrollLocked = true;
     scrollTop = scrollPosition.top;
   }
 }
+
 /**
  * @type {function}
  * @ignore
  * @deprecated
  */
-
 var enable = enableScrollLock;
+
 /**
  * Disable the scroll lock
  */
-
 function disableScrollLock() {
   if (isScrollLocked) {
     var scrollPosition = getElementScroll();
@@ -306,22 +307,26 @@ function disableScrollLock() {
     htmlTag.style.marginTop = '';
     htmlTag.style.position = '';
     htmlTag.style.overflow = '';
-    htmlTag.style.width = ''; // Set the scroll position to what it was before
+    htmlTag.style.width = '';
 
-    window.scrollTo(scrollPosition.left, scrollTop); // Trigger event on target. You can listen for it using document.body.addEventListener("site.scrollLock:disable", callbackHere)
+    // Set the scroll position to what it was before
+    window.scrollTo(scrollPosition.left, scrollTop);
+
+    // Trigger event on target. You can listen for it using document.body.addEventListener("site.scrollLock:disable", callbackHere)
     // (document.body, "site.scrollLock:disable");
-    // Remember state
 
+    // Remember state
     isScrollLocked = false;
   }
 }
+
 /**
  * @type {function}
  * @ignore
  * @deprecated
  */
-
 var disable = disableScrollLock;
+
 /**
  * Site some global responsive site settings.
  *
@@ -348,17 +353,11 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _global__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(2);
 /* harmony import */ var _utils__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(3);
 function _toConsumableArray(arr) { return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _unsupportedIterableToArray(arr) || _nonIterableSpread(); }
-
 function _nonIterableSpread() { throw new TypeError("Invalid attempt to spread non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
-
 function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
-
 function _iterableToArray(iter) { if (typeof Symbol !== "undefined" && iter[Symbol.iterator] != null || iter["@@iterator"] != null) return Array.from(iter); }
-
 function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) return _arrayLikeToArray(arr); }
-
 function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
-
 /*
 Luke Harby
 slackwise LTD
@@ -369,7 +368,6 @@ https://slackwise.org.uk
 /* navigation */
 
 
-
 var navigation = function navigation() {
   var wrapper = 'nav-wrapper';
   var target = _global__WEBPACK_IMPORTED_MODULE_0__["DOM"].querySelector(".".concat(wrapper));
@@ -377,11 +375,9 @@ var navigation = function navigation() {
   var trigger = _global__WEBPACK_IMPORTED_MODULE_0__["DOM"].querySelectorAll('.menu__toggle');
   var overlayOpenClass = 'overlay__open';
   var scrollLockClass = 'scroll-lock';
-
   _toConsumableArray(trigger).forEach(function (item) {
     item.addEventListener('click', function (event) {
       event.preventDefault();
-
       if (item.classList.contains('menu__toggle--close')) {
         closeNavigation();
       } else {
@@ -389,7 +385,6 @@ var navigation = function navigation() {
       }
     });
   });
-
   overlay.addEventListener('click', function () {
     if (overlay.classList.contains(overlayOpenClass)) {
       closeNavigation();
@@ -400,14 +395,12 @@ var navigation = function navigation() {
       closeNavigation();
     }
   });
-
   var openNavigation = function openNavigation() {
     target.classList.add("".concat(wrapper, "--open"));
     _global__WEBPACK_IMPORTED_MODULE_0__["BODY"].classList.add(scrollLockClass);
     overlay.classList.add(overlayOpenClass);
     Object(_utils__WEBPACK_IMPORTED_MODULE_1__["enableScrollLock"])();
   };
-
   var closeNavigation = function closeNavigation() {
     target.classList.remove("".concat(wrapper, "--open"));
     _global__WEBPACK_IMPORTED_MODULE_0__["BODY"].classList.remove(scrollLockClass);
@@ -415,7 +408,6 @@ var navigation = function navigation() {
     Object(_utils__WEBPACK_IMPORTED_MODULE_1__["disableScrollLock"])();
   };
 };
-
 /* harmony default export */ __webpack_exports__["default"] = (navigation);
 
 /***/ }),
@@ -434,13 +426,11 @@ https://slackwise.org.uk
 
 /* index page */
 
-
 var indexPage = function indexPage() {
   var url = document.location.pathname.split('/');
   var primaryDir = url[1];
   var secondaryDir = url[2];
   var tertiaryDir = url[3];
-
   if (!primaryDir) {
     _global__WEBPACK_IMPORTED_MODULE_0__["DOM"].classList.add('index');
   } else if (tertiaryDir) {
@@ -451,7 +441,6 @@ var indexPage = function indexPage() {
     _global__WEBPACK_IMPORTED_MODULE_0__["DOM"].classList.add(primaryDir);
   }
 };
-
 /* harmony default export */ __webpack_exports__["default"] = (indexPage);
 
 /***/ }),
@@ -470,13 +459,11 @@ https://slackwise.org.uk
 
 /* date function */
 
-
 var date = function date() {
   var dateWrapper = _global__WEBPACK_IMPORTED_MODULE_0__["DOM"].querySelector('.date-wrapper');
   var getYear = new Date().getFullYear();
   dateWrapper.textContent = getYear;
 };
-
 /* harmony default export */ __webpack_exports__["default"] = (date);
 
 /***/ }),
@@ -500,11 +487,9 @@ https://slackwise.org.uk
 
 
 var fadeOutClass = 'fade-out';
-
 var entry = function entry() {
   var cookieIsSet = Object(_cookies__WEBPACK_IMPORTED_MODULE_2__["getCookie"])('entry');
   var entryElem = document.querySelector('.entry');
-
   if (cookieIsSet) {
     entryElem.classList.add(_global__WEBPACK_IMPORTED_MODULE_0__["HIDDEN_CLASS"], _global__WEBPACK_IMPORTED_MODULE_0__["VISIBILITY_HIDDEN_CLASS"]);
     Object(_utils__WEBPACK_IMPORTED_MODULE_1__["disableScrollLock"])();
@@ -512,7 +497,6 @@ var entry = function entry() {
     entryElem.classList.remove(_global__WEBPACK_IMPORTED_MODULE_0__["HIDDEN_CLASS"], _global__WEBPACK_IMPORTED_MODULE_0__["VISIBILITY_HIDDEN_CLASS"]);
     Object(_utils__WEBPACK_IMPORTED_MODULE_1__["enableScrollLock"])();
   }
-
   entryElem.addEventListener('click', function (e) {
     e.preventDefault();
     entryElem.classList.add(fadeOutClass);
@@ -524,7 +508,6 @@ var entry = function entry() {
     });
   });
 };
-
 /* harmony default export */ __webpack_exports__["default"] = (entry);
 
 /***/ }),
@@ -542,31 +525,26 @@ slackwise LTD
 https://slackwise.org.uk
 2012 - present
 */
+
 function setCookie(name, value, days) {
   var expires = "";
-
   if (days) {
     var date = new Date();
     date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
     expires = "; expires=" + date.toUTCString();
   }
-
   document.cookie = name + "=" + (value || "") + expires + "; path=/";
 }
 function getCookie(name) {
   var nameEQ = name + "=";
   var ca = document.cookie.split(';');
-
   for (var i = 0; i < ca.length; i++) {
     var c = ca[i];
-
     while (c.charAt(0) == ' ') {
       c = c.substring(1, c.length);
     }
-
     if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
   }
-
   return null;
 }
 function deleteCookie(name) {
@@ -589,7 +567,6 @@ https://slackwise.org.uk
 
 /* toggle contrast function */
 
-
 var toggleContrast = function toggleContrast() {
   var trigger = _global__WEBPACK_IMPORTED_MODULE_0__["DOM"].querySelector('.toggle-contrast');
   var contrastClass = 'high-contrast';
@@ -597,7 +574,6 @@ var toggleContrast = function toggleContrast() {
   lowContrast === '0' ? _global__WEBPACK_IMPORTED_MODULE_0__["DOM"].classList.remove(contrastClass) : null;
   trigger.addEventListener('click', function (event) {
     event.preventDefault();
-
     if (!_global__WEBPACK_IMPORTED_MODULE_0__["DOM"].classList.contains(contrastClass)) {
       _global__WEBPACK_IMPORTED_MODULE_0__["DOM"].classList.add(contrastClass);
       window.localStorage.removeItem(contrastClass);
@@ -607,7 +583,6 @@ var toggleContrast = function toggleContrast() {
     }
   });
 };
-
 /* harmony default export */ __webpack_exports__["default"] = (toggleContrast);
 
 /***/ }),
@@ -626,7 +601,6 @@ https://slackwise.org.uk
 
 /* obfuscate email */
 
-
 var obfuscateEmail = function obfuscateEmail() {
   var trigger = _global__WEBPACK_IMPORTED_MODULE_0__["DOM"].querySelectorAll('.email-address');
   trigger.forEach(function (item) {
@@ -635,7 +609,6 @@ var obfuscateEmail = function obfuscateEmail() {
     });
   });
 };
-
 /* harmony default export */ __webpack_exports__["default"] = (obfuscateEmail);
 
 /***/ }),
@@ -655,7 +628,6 @@ https://slackwise.org.uk
 /* append form */
 
 var template = "\n    <form action=\"\" method=\"post\" class=\"form\">\n        <div class=\"form-control\">\n            <label class=\"el-4\" for=\"actual_name\">Name</label>\n            <input class=\"el-5\" type=\"text\" name=\"actual_name\" id=\"actual_name\">\n            <input name=\"name\" type=\"text\" id=\"name\" class=\"hidden\">\n        </div>\n        <div class=\"form-control\">\n            <label class=\"el-4\" for=\"email\">Email</label>\n            <input class=\"el-5\" type=\"text\" name=\"email\" id=\"email\">\n        </div>\n        <div class=\"form-control\">\n            <label class=\"el-4\" for=\"contact-no\">Contact number</label>\n            <input class=\"el-5\" type=\"text\" id=\"contact-no\">\n        </div>\n        <div class=\"form-control\">\n            <label class=\"el-4\" for=\"comments\">Comments</label>\n            <textarea class=\"el-5\" name=\"comments\" id=\"comments\" rows=\"5\"></textarea>\n        </div>\n        <div class=\"form-control\">\n            <div class=\"el-4 mobile-hidden\"></div>\n            <div class=\"el-5\">\n                <button class=\"submit\">Submit</button>\n            </div>\n        </div>\n    </form>\n";
-
 var appendForm = function appendForm() {
   var wrapper = _global__WEBPACK_IMPORTED_MODULE_0__["DOM"].querySelector('.form-wrapper');
   wrapper.innerHTML = template;
@@ -668,7 +640,6 @@ var appendForm = function appendForm() {
     }
   });
 };
-
 /* harmony default export */ __webpack_exports__["default"] = (appendForm);
 
 /***/ }),
@@ -688,7 +659,6 @@ https://slackwise.org.uk
 var getScrollBarWidth = function getScrollBarWidth() {
   return document.documentElement.style.setProperty('--scrollbar-width', window.innerWidth - document.documentElement.clientWidth + 'px');
 };
-
 /* harmony default export */ __webpack_exports__["default"] = (getScrollBarWidth);
 
 /***/ }),
@@ -707,13 +677,11 @@ https://slackwise.org.uk
 
 /* lazyload function */
 var images = document.querySelectorAll('#posts img');
-
 var observeImages = function observeImages() {
   images.forEach(function (img) {
     observer.observe(img);
   });
 };
-
 var setImageSource = function setImageSource() {
   images.forEach(function (item) {
     var newSrc = item.src.replace('500', '1280');
@@ -721,12 +689,10 @@ var setImageSource = function setImageSource() {
   });
   observeImages();
 };
-
 var options = {
   rootMargin: '1px',
   threshold: 0.1
 };
-
 var lazyLoad = function lazyLoad(elements) {
   if (elements.length) {
     elements.forEach(function (item) {
@@ -738,7 +704,6 @@ var lazyLoad = function lazyLoad(elements) {
     });
   }
 };
-
 var observer = new IntersectionObserver(lazyLoad, options);
 
 
@@ -762,18 +727,15 @@ https://slackwise.org.uk
 
 var colours = ['#ee4800', '#60c600', '#00ffff', '#ff7f00', '#ff6ccc', '#848382', '#d2dd26'];
 var rndColour = Math.floor(Math.random() * colours.length);
-
 var setRandomColour = function setRandomColour() {
   document.documentElement.style.setProperty('--selection-background', colours[rndColour]);
 };
-
 var attachClickEvent = function attachClickEvent() {
   var menuOpen = _global__WEBPACK_IMPORTED_MODULE_0__["DOM"].querySelector('.menu__toggle');
   var menuClose = _global__WEBPACK_IMPORTED_MODULE_0__["DOM"].querySelector('.menu__toggle--close');
   menuOpen.click();
   menuClose.click();
 };
-
 
 
 /***/ })
